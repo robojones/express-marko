ab75a4336172f3c4e5953299e58f8446b0aa3952
robojones <jonathan.scholz-weil@t-online.de>
2017-02-27 20:59:18 +0100

    add support for $global

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ require('marko/node-require').install()
 
 module.exports = (req, res, next) => {
   const views = path.resolve(req.app.get('views'))
+  res.global = {}
 
   res.render = (view, locals = {}, callback = res) => {
     const template = path.join(views, view)
     if(process.env.NODE_ENV === 'development') {
       delete require.cache[require.resolve(template)]
     }
-    require(template).render(locals, callback)
+    const data = Object.assign({}, locals, {
+      $global: Object.assign({}, locals.$global, res.global)
+    })
+
+    require(template).render(data, callback)
   }
 
   next()
 }
-
